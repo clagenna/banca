a8cba8de7ad0a5d0ced7acd2702861ecde30e4a1
clagenna <clagenna@gmail.com>
2026-07-03 17:35:55 +0200

    Raggiunto la versione 1.0.11

--- src/main/java/sm/clagenna/banca/dati/Versione.java
+++ src/main/java/sm/clagenna/banca/dati/Versione.java
@@ -28,10 +28,10 @@ public class Versione implements Serializable {
   /** Minor Version */
   public static final int APP_MIN_VERSION = 0;
   /** Build Version */
-  public static final int    APP_BUILD = 10;
+  public static final int    APP_BUILD = 11;
 
   // e oggi esteso ${dh:CSZ_DATEDEPLOY}
-  public static final String CSZ_DATEDEPLOY = "03/07/2026 17:31:14";
+  public static final String CSZ_DATEDEPLOY = "03/07/2026 17:35:54";
 
   public static void main(String[] args) {
     System.out.println(DESC_APPL + " " + Versione.getVersion());
@@ -70,6 +70,7 @@ public class Versione implements Serializable {
     return sz;
   }
 }
+
 
 
 
